3c4e8610437060d370e6f6575ff4014fcbac6ca0
SerBakon <134323205+SerBakon@users.noreply.github.com>
2025-09-23 22:25:38 -0700

    feat: cart module

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,19 @@
 export const cart = [];
+
+
+export function addToCart(productId) {
+    let matchingItem;
+    cart.forEach((cartItem) => {
+        if(cartItem.productId === productId) {
+            matchingItem = cartItem;
+            //console.log('matching item found');
+        }
+    });
+
+    //console.log(`matching item: ${matchingItem}`);
+    if(matchingItem) {
+        matchingItem.quantity += 1;
+    } else {
+        cart.push({productId: productId, quantity: 1});
+    }
+}
